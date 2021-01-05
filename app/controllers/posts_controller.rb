8895a3770a88a5ee2ec64efda2ebba9fbf58ccb4
iwatamasayuki <sorhoun3@gmail.com>
2021-01-05 12:07:08 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  def index 
+  def index
     @posts = Post.all.order(id: "DESC")  # 1番目のレコードを@postに代入 /idをDESC（降順）で並び替える
   end
 
@@ -7,6 +7,18 @@ class PostsController < ApplicationController
   def create
     Post.create(content: params[:content])
     redirect_to action: :index
+  end
+
+  def checked
+    post = Post.find(params[:id])
+    if post.checked
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
   end
 end
 
